df4f9eb0c17e097d44f40cdb9817c9e1b2d93ec4
Sushant Chougule <sushantchougule0206@gmail.com>
2024-05-18 01:41:33 +0530

    java commits

---
+++ Day5.2_inheritancePayroll/src/com/iacsd/SalesPerson.java
@@ -0,0 +1,39 @@
+package com.iacsd;
+
+
+//BaseClass:
+public final class SalesPerson extends Employee{
+
+	private int pa,fa,ta;
+	
+	public SalesPerson()
+	{
+		//super()
+		System.out.println("---default constr SalesPerson----");
+		pa=fa=ta=500;
+	}
+	
+	public SalesPerson(int empid,String name,double salary,int pa,int fa,int ta)
+	{
+		super(empid,name,salary);//parameter constr of Employee
+		
+		System.out.println("----parameter constr Salesperson------");
+		this.pa=pa;
+		this.ta=ta;
+		this.fa=fa;
+	}
+	
+	
+	@Override
+	public void calSalary()
+	{
+		System.out.println("SalesPerson:="+(super.salary+pa+fa+ta));
+	}
+
+	@Override
+	public void show() {
+		super.show();
+		System.out.println("SalesPerson "+pa+"  "+fa+"    "+ta);
+	}	
+	
+}
